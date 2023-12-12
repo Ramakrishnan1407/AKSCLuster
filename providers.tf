c9fdb6d1a7e2ecf5e896b086feee0eed4da39203
ramakrishnan1407 <raminzenith@yahoo.co.in>
2023-12-12 20:36:41 +0100

    AKS helm

--- providers.tf
+++ providers.tf
@@ -23,3 +23,18 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+data "azurerm_kubernetes_cluster" "aks" {
+  name                = azurerm_kubernetes_cluster.aks.name
+  resource_group_name = azurerm_resource_group.default.name
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.azurerm_kubernetes_cluster.aks.kube_config.0.host
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
+
+  }
+}
