9af46d4e079526658d897f9934c50245656f03e2
ramakrishnan1407 <raminzenith@yahoo.co.in>
2024-01-03 13:18:58 +0100

    Helm Deploy

--- main.tf
+++ main.tf
@@ -44,8 +44,9 @@ resource "azurerm_container_registry" "acr_platform_shared" {
 
 resource "helm_release" "my-chart" {
   name                = "test-dev"
-  repository          = "https://acrtest1407.azurecr.io/helm/v1/repo"
+  repository          = "https://acrtest1407.azurecr.io/becse/shap/mule/chart/hello-world"
   repository_username = "f024578a-6e8e-479b-a7b5-22c0d278e43f"
   repository_password = "V6n8Q~jFFIyk1Uu-r~Rsu2SR_PPW1kI-gYOGVdsw"
-  chart               = "becse/shap/mule/chart/hello-world"
+  chart               = "hello-world"
+  version             = "0.1.0"
 }
