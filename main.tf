5f7584631d6f9033b1b6415f993b6fd52becb57a
rrajaram2 <rrajaram2@delhaize.be>
2024-03-07 17:49:56 +0100

    feat: extra AKS

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "agentpool"
     temporary_name_for_rotation = "temp"
-    vm_size    = "standard_ds4_v2"
+    vm_size    = "standard_a2_v2"
     node_count = 2
   }
 
