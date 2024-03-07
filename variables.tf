b4ad31209a722de9d46a87952922feb7451b24c9
ramakrishnan1407 <raminzenith@yahoo.co.in>
2024-03-07 10:56:34 +0100

    create AKS cluster

--- variables.tf
+++ variables.tf
@@ -1,16 +1,7 @@
 variable "resource_group" {
   type        = string
   description = "Platform owner AAD security group"
-  default = {
-    projects:
-  - code: conn
-    application_ingresses:
-      - application_name: shapconnect
-        public: false
-        path: /
-        path_service_name: shapconnect
-        path_service_port_number: 80
   }
-}
+
 
 
