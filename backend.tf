f51951e8093599e47336b87952accb7c8b758d8f
ramakrishnan1407 <raminzenith@yahoo.co.in>
2024-04-02 15:02:30 +0200

    feat: Test AKS

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "terraformstate"
+    resource_group_name  = "myakscluster"
     storage_account_name = "terraformstate1407"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
