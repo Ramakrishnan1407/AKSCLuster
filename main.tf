e4def4865ca1831ac3cad068e8ac458b26dd661b
Ramakrishnan1407 <72505656+Ramakrishnan1407@users.noreply.github.com>
2024-06-18 10:19:31 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -105,4 +105,32 @@ resource "azurerm_subnet" "example" {
   address_prefixes     = ["10.254.0.0/24"]
 }
 
+resource "azurerm_key_vault" "example" {
+  name                        = "examplekeyvault"
+  location                    = azurerm_resource_group.example.location
+  resource_group_name         = azurerm_resource_group.example.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
 
