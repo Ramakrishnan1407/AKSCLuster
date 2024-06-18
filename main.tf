cd703c622a1a3f49f8f4df955eb799143c5eccd2
Ramakrishnan1407 <72505656+Ramakrishnan1407@users.noreply.github.com>
2024-06-18 10:00:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "default" {
-  name     = "myaksclusternew"
+  name     = "myaksclusternewram"
   location = "westeurope"
 
   tags = {
@@ -60,6 +60,7 @@ resource "azurerm_container_registry" "acr_platform_shared" {
   sku                 = "Standard"
   admin_enabled       = true
 }
+*/
 
 /*
 resource "helm_release" "hello-world" {
@@ -86,7 +87,7 @@ resource "null_resource" "aks_upgrade" {
 }*/
 
 resource "azurerm_resource_group" "example" {
-  name     = "appgateway"
+  name     = "appgatewayram"
   location = "West Europe"
 }
 
@@ -104,77 +105,4 @@ resource "azurerm_subnet" "example" {
   address_prefixes     = ["10.254.0.0/24"]
 }
 
-resource "azurerm_public_ip" "example" {
-  name                = "example-pip"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  allocation_method   = "Dynamic"
-}
-
-# since these variables are re-used - a locals block makes this more maintainable
-locals {
-  backend_address_pool_name      = "${azurerm_virtual_network.example.name}-beap"
-  frontend_port_name             = "${azurerm_virtual_network.example.name}-feport"
-  frontend_ip_configuration_name = "${azurerm_virtual_network.example.name}-feip"
-  http_setting_name              = "${azurerm_virtual_network.example.name}-be-htst"
-  listener_name                  = "${azurerm_virtual_network.example.name}-httplstn"
-  request_routing_rule_name      = "${azurerm_virtual_network.example.name}-rqrt"
-  redirect_configuration_name    = "${azurerm_virtual_network.example.name}-rdrcfg"
-}
-
-resource "azurerm_application_gateway" "network" {
-  name                = "example-appgateway"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-
-  sku {
-    name     = "Standard_v2"
-    tier     = "Standard_v2"
-    capacity = 2
-  }
-
-  gateway_ip_configuration {
-    name      = "my-gateway-ip-configuration"
-    subnet_id = azurerm_subnet.example.id
-  }
-
-  frontend_port {
-    name = local.frontend_port_name
-    port = 80
-  }
-
-  frontend_ip_configuration {
-    name                 = local.frontend_ip_configuration_name
-    public_ip_address_id = azurerm_public_ip.example.id
-  }
-
-  backend_address_pool {
-    name = local.backend_address_pool_name
-  }
-
-  backend_http_settings {
-    name                  = local.http_setting_name
-    cookie_based_affinity = "Disabled"
-    path                  = "/path1/"
-    port                  = 80
-    protocol              = "Http"
-    request_timeout       = 60
-  }
-
-  http_listener {
-    name                           = local.listener_name
-    frontend_ip_configuration_name = local.frontend_ip_configuration_name
-    frontend_port_name             = local.frontend_port_name
-    protocol                       = "Http"
-  }
-
-  request_routing_rule {
-    name                       = local.request_routing_rule_name
-    priority                   = 9
-    rule_type                  = "Basic"
-    http_listener_name         = local.listener_name
-    backend_address_pool_name  = local.backend_address_pool_name
-    backend_http_settings_name = local.http_setting_name
-  }
-}  
 
