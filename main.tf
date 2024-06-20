46f1c124e4c22d6ee579ec45a455e0a53932f6fa
Ramakrishnan1407 <72505656+Ramakrishnan1407@users.noreply.github.com>
2024-06-20 12:31:35 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -108,7 +108,7 @@ resource "azurerm_subnet" "example" {
 }
 
 resource "azurerm_key_vault" "example" {
-  name                        = "examplevaultram1408"
+  name                        = "examplevaultram1409"
   location                    = azurerm_resource_group.example.location
   resource_group_name         = azurerm_resource_group.example.name
   enabled_for_disk_encryption = true
